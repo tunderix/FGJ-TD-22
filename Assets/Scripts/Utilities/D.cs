46b7705b211bd3b2c30c49f6a5c98320c95a5594
Ionisaatio <sami.repo@live.com>
2022-01-28 09:49:18 +0200

    debug Logging enhanced

--- Assets/Scripts/Utilities/D.cs
+++ Assets/Scripts/Utilities/D.cs
@@ -4,53 +4,107 @@ using Object = UnityEngine.Object;
 
 namespace Creator.Utilities
 {
-    public class D
+    /// <summary>
+    /// D - Logging
+    ///
+    /// Custom wrapper for default unity debug console. 
+    /// </summary>
+    public static class D
     {
-        /// <summary>
-        ///   <para>Logs a message to the Unity Console.</para>
-        /// </summary>
-        /// <param name="message">String or object to be converted to string representation for display.</param>
-        public static void Info(object message) => Debug.Log(message);
+        private static string Color(this string myStr, string color)
+        {
+            return string.IsNullOrEmpty(myStr) ?
+             "" : $"<color={color}>{myStr}</color>";
+        }
+        
+        private static string Message(string prefix, Object obj, params object[] message)
+        {
+            var objName = obj != null 
+                ? $"<b>{obj.name.Color("lightblue")}</b>" 
+                : String.Empty;
+            var prefixes = !String.IsNullOrEmpty(prefix) && obj != null
+                ? $"{prefix.Color("lightblue")}"
+                : String.Empty;
+            var separator = String.IsNullOrEmpty(objName) && String.IsNullOrEmpty(prefixes) ? String.Empty : ": ";
+            var messages = string.Join(" | ", message);
+            return $"{prefixes}{objName}{separator}{messages}";
+        }
         
         /// <summary>
         ///   <para>Logs a message to the Unity Console.</para>
         /// </summary>
         /// <param name="message">String or object to be converted to string representation for display.</param>
+        public static void Info(params object[] message)
+        {
+#if UNITY_EDITOR
+            Debug.Log(Message("Info - ", null, message)); 
+#endif
+        }
+
+        /// <summary>
+        ///   <para>Logs a message to the Unity Console.</para>
+        /// </summary>
+        /// <param name="message">String or object to be converted to string representation for display.</param>
         /// <param name="context">Object to which the message applies.</param>
-        public static void Info(object message, Object context) => Debug.Log(message, context);
+        public static void Info(Object context, params object[] message)
+        {
+#if UNITY_EDITOR
+            Debug.Log(Message("Info - ", context, message)); 
+#endif
+        }
         
         /// <summary>
         ///   <para>A variant of Debug.Log that logs an error message to the console.</para>
         /// </summary>
         /// <param name="message">String or object to be converted to string representation for display.</param>
-        public static void Err(object message) => Debug.LogError(message);
+        public static void Err(params object[] message)
+        {
+#if UNITY_EDITOR
+            Debug.LogError(Message("Error - ", null, message)); 
+#endif
+        }
         
         /// <summary>
         ///   <para>A variant of Debug.Log that logs an error message to the console.</para>
         /// </summary>
         /// <param name="message">String or object to be converted to string representation for display.</param>
         /// <param name="context">Object to which the message applies.</param>
-        public static void Err(object message, Object context) => Debug.LogError(message, context);
+        public static void Err(Object context, params object[] message)
+        {
+#if UNITY_EDITOR
+            Debug.LogError(Message("Error - ", context, message)); 
+#endif
+        }
         
         /// <summary>
         ///   <para>A variant of Debug.Log that logs a warning message to the console.</para>
         /// </summary>
         /// <param name="message">String or object to be converted to string representation for display.</param>
-        public static void Warn(object message) => Debug.LogWarning(message);
+        public static void Warn(params object[] message)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning(Message("Warn - ", null, message)); 
+#endif
+        }
         
         /// <summary>
         ///   <para>A variant of Debug.Log that logs a warning message to the console.</para>
         /// </summary>
         /// <param name="message">String or object to be converted to string representation for display.</param>
         /// <param name="context">Object to which the message applies.</param>
-        public static void Warn(object message, Object context) => Debug.LogWarning(message, context);
+        public static void Warn(Object context, params object[] message)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning(Message("Warn - ", context, message)); 
+#endif
+        }
         
         /// <summary>
         ///   <para>A variant of Debug.Log that logs an error message to the console.</para>
         /// </summary>
         /// <param name="exception">Runtime Exception.</param>
         public static void Exception(Exception exception) => Debug.LogException(exception);
-        
+
         /// <summary>
         ///   <para>A variant of Debug.Log that logs an error message to the console.</para>
         /// </summary>
