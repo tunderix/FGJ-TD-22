31072e13417275832b86d78fa66d958253d60046
Ionisaatio <sami.repo@live.com>
2022-01-29 10:25:20 +0200

    Fixed raycasting for tower placement

--- Assets/Scripts/Player/TowerPlacer.cs
+++ Assets/Scripts/Player/TowerPlacer.cs
@@ -8,20 +8,22 @@ namespace Creator.Player
     public class TowerPlacer : MonoBehaviour
     {
         private Vector3 towerPlacement; 
+        [SerializeField] private LayerMask layerMask;
         
         private void FixedUpdate()
         {
+            // layerMask = ~layerMask;
             RaycastHit hit;
-            // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask, QueryTriggerInteraction.Ignore))
             {
                 Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-                towerPlacement = hit.point; 
             }
             else
             {
-                towerPlacement = Vector3.zero;
+                //hit = Vector3.zero;
             }
+
+            towerPlacement = hit.point; 
         }
 
         public Vector3 GetTowerPlacement() => this.towerPlacement;
